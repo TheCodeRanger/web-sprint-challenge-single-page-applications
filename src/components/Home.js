df87a8d58f3b71991953cfb90806b59bbbe34f5b
Christopher Greene <greenebasecamp@gmail.com>
2021-02-22 15:17:17 -0500

    Term2

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,10 @@ import Styling from '../styling/home.css';
 //ALL home page content here. Reference forms guided project.
 export const Home = () => {
     return (
-        <div>
-            <h1>Welcome to ARNOLD's PIZZA SHOP</h1>
-            <h2>"Home of the 9MM bullet pit-zah, you id-iot."</h2>
-            <p>Get your pie NOW!... or I come to your house and punch it through your forehead.</p>
+        <div className='welcome'>
+            <h1 className='title'>ARNOLD'S PIZZA SHOP</h1>
+            <h2 className='motto'>"Home of the 9MM bullet pit-zah, you id-iot."</h2>
+            <p className='phrase'>Get your pie NOW!... or I come to your house and punch it through your forehead.</p>
         </div>
     )
 };
